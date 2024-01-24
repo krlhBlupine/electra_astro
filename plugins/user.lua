7000b55cc894d9c6f0f6edbe604c99a4fc913d68
Blupine <krlh.blupine@proton.me>
2024-01-23 22:03:56 -0600

    add user plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,35 @@
 return {
- { "catppuccin/nvim", name = "catppuccin", priority = 1000 } -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+ { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, -- You can also add new plugins here as well:
+ { 'rose-pine/neovim', name = 'rose-pine', priority = 1000 },
+ { 'aspeddro/pandoc.nvim', name = 'pandoc'},
+ {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+ {'ekickx/clipboard-image.nvim', name = 'clipboard-image'},
+ {'tpope/vim-repeat', name = 'vim-repeat', priority = 1000, lazy = false},
+ {"tpope/vim-surround", name = 'vim-surround', priority = 1000, lazy = false},
+ {'jakewvincent/mkdnflow.nvim',
+        name = 'mkdnflow',
+        lazy = false,
+        config = function()
+            require('mkdnflow').setup({
+                mappings = {
+                   MkdnEnter = {{'i', 'n', 'v'}, '<CR>'}
+                },
+                links = {
+                    transform_explicit = function(input)
+                                            return('../'..input)
+                                            end,
+                    transform_implicit = function(input)
+                                              return(string.gsub(input, '../', ''))
+                                              end,
+                    name_is_source = true,
+                    conceal = true,
+                    }
+                -- Config goes here; leave blank for defaults
+            })
+        end,
+ }
+ -- Add plugins, the lazy syntax
+ -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
